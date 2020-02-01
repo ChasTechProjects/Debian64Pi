425046451abedb18e65f79de618841f4ded32686
Craigory <etdsaunders@gmail.com>
2020-02-01 13:25:50 +0000

    zram-tools: updates
    So we installed zram tools, but only gives you 256mb swap.
    
    We've now made changes so it removes config and gives you more.  If you have an RPi with 4GB ram, you'll get 8gb swap

--- stage2.sh
+++ stage2.sh
@@ -5,6 +5,26 @@ apt upgrade -y
 apt install console-setup keyboard-configuration sudo ssh curl wget dbus usbutils ca-certificates crda less fbset debconf-utils avahi-daemon fake-hwclock nfs-common apt-utils man-db pciutils openssh-server zram-tools ntfs-3g apt-listchanges -y
 apt install wpasupplicant wireless-tools firmware-atheros firmware-brcm80211 firmware-libertas firmware-misc-nonfree firmware-realtek dhcpcd5 net-tools -y
 
+# Setup zram-tools - swap will be 2 x ram amount. 1GB ram, will give you 2gb zram
+
+rm /etc/default/zramswap
+
+echo '# Specifies amount of zram devices to create.
+# By default, zramswap-start will use all available cores.
+#CORES=1
+
+# Specifies the amount of RAM that should be used for zram
+# based on a percentage the total amount of available memory
+PERCENTAGE=200
+
+# Specifies a static amount of RAM that should be used for
+# the ZRAM devices, this is in MiB
+#ALLOCATION=256
+
+# Specifies the priority for the swap devices, see swapon(2)
+# for more details.
+#PRIORITY=100' >> /etc/default/zramswap
+
 # Uncomment one of these to install a Desktop Environment
 
 #apt install task-xfce-desktop -y # For XFCE
